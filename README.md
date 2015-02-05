e7d3926d9a9caa13dc2bc083da1074cff061c10c
Pieter Koornhof <pieter@entelect.co.za>
2015-02-05 08:37:04 +0200

    Add Entities

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ This is a companion project to the Practical Domain Driven Design articles poste
 It gets updated in parallel with the above mentioned articles.
 
 Eventually this project will be a usable project template where the parts that are not needed can be easily pruned.
+
+___
+
+##Architecture
+[Intro to Practical DDD](http://sneakycode.net/intro-to-practical-domain-driven-design/)
+
+[App Structure p1](http://sneakycode.net/domain-driven-design-app-structure/)
+
+[App Structure p2](http://sneakycode.net/domain-driven-design-app-structure-part-2/)
+
+##Entities
+[Entity is For Identity](http://sneakycode.net/entity-is-for-identity/)
---
+++ src/SimpleDDD.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,52 @@
+﻿namespace SimpleDDD.Domain.Abstractions
+{
+    //Todo Choose an entity below that suits your needs and rename it to IEntity
+    //http://sneakycode.net/entity-is-for-identity/
+
+    /// <summary>
+    /// An Entity is responsible for tracking it's state and the rules regulating it's lifecycle.
+    /// It is identified by it's Id and not by other attributes
+    /// Use if various identity types are going to be used.
+    /// </summary>
+    /// <typeparam name="TKey">Identity Type</typeparam>
+    public interface IEntity<out TKey>
+    {
+        TKey Id { get; }
+    }
+
+    /// <summary>
+    /// An Entity is responsible for tracking it's state and the rules regulating it's lifecycle.
+    /// It is identified by it's Id and not by other attributes
+    /// Use if single identity type is used across all entities.
+    /// </summary>
+    public interface ISimpleEntity
+    {
+        int Id { get; }
+    }
+
+    /// <summary>
+    /// An Entity is responsible for tracking it's state and the rules regulating it's lifecycle.
+    /// It is identified by it's Id and not by other attributes
+    /// Use if Identity comparison is not trivial
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IComparableEntity<out TKey, in TEntity>
+    {
+        TKey Id { get; }
+
+        bool IsSameAs(TEntity entity);
+    }
+
+    /// <summary>
+    /// An Entity is responsible for tracking it's state and the rules regulating it's lifecycle.
+    /// It is identified by it's Id and not by other attributes
+    /// Use if Identity comparison is not trivial. But not all Entities require complex comparison
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface IComparableInheritingEntity<out TKey, in TEntity> : IEntity<TKey>
+    {
+        bool IsSameAs(TEntity entity);
+    }
+}
